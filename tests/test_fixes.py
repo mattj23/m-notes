70116d61cd7575da9eef8d4463758574911db7d0
Matt Jarvis <mattj23@gmail.com>
2021-02-22 00:51:00 -0500

    caught some missing paths in tests

--- tests/test_fixes.py
+++ tests/test_fixes.py
@@ -9,7 +9,7 @@ from mnotes.notes.markdown_notes import NoteBuilder, NoteInfo
 from mnotes.utility.change import ChangeTransaction
 from tests.test_index import local_tz
 
-from mnotes.fix.common import CreationFixer, IdFixer, AuthorFixer, TitleFixer
+from mnotes.fix.common import CreationFixer, IdFixer, AuthorFixer, TitleFixer, FilenameFixer
 from mnotes.notes.markdown_notes import ID_TIME_FORMAT
 from tests.tools.file_system_mocks import TestFileSystemProvider
 from datetime import datetime as DateTime
@@ -100,200 +100,195 @@ def test_created_read_file_time(fixture):
     assert copy.content == note2.content
 
 
-def test_id_check_true(check_fixture):
-    provider, builder = check_fixture
-    note = builder.note_builder.load_note("/fix/missing-id-only.md")
-    fixer = IdFixer(builder.note_builder, False)
+def test_id_check_true(fixture):
+    note = fixture.master.get_note_info("/fix/missing-id-only.md")
+    fixer = IdFixer(fixture.index_builder.note_builder, False)
 
-    assert fixer.check(note.info)
-
-
-def test_id_check_false(check_fixture):
-    provider, builder = check_fixture
-    note = builder.note_builder.load_note("/alpha/note-00.md")
-    fixer = IdFixer(builder.note_builder, False)
-
-    assert not fixer.check(note.info)
+    assert fixer.check(note)
 
 
-def test_id_fix_simple(transact_fixture):
-    provider, builder, master = transact_fixture
-    master.load_all()
-    transaction = master.create_empty_transaction()
+def test_id_check_false(fixture):
+    note = fixture.master.get_note_info("/alpha/note-00.md")
+    fixer = IdFixer(fixture.index_builder.note_builder, False)
 
-    note = builder.note_builder.load_note("/fix/missing-id-only.md")
-    fixer = IdFixer(builder.note_builder, False)
-    result = fixer.try_change(note.info, transaction)
-    result.change.change_maker.apply_change(result.change)  # convoluted, but to ensure that it's stand-alone
-    note2 = builder.note_builder.load_note("/fix/missing-id-only.md")
+    assert not fixer.check(note)
+
+
+def test_id_fix_simple(fixture):
+    file = "/fix/missing-id-only.md"
+    copy = deepcopy(fixture.master.get_note(file))
+    fixer = IdFixer(fixture.index_builder.note_builder, False)
+    result = fixer.try_change(file, fixture.transact)
+    fixture.transact.add_change(file, result.change)
+    fixture.master.apply_transaction(fixture.transact)
+
+    note2 = fixture.index_builder.note_builder.load_note(file)
 
     assert result.is_ok
     assert note2.info.id == "20190728215833"
-    assert but_for(note.info, "id") == but_for(note2.info, "id")
-    assert note2.content == note.content
+    assert but_for(copy.info, "id") == but_for(note2.info, "id")
+    assert copy.content == note2.content
 
 
-def test_id_refuse_existing_conflict(transact_fixture):
-    provider, builder, master = transact_fixture
-    master.load_all()
-    transaction = master.create_empty_transaction()
-
-    note = builder.note_builder.load_note("/fix/missing-id-conflict.md")
-    fixer = IdFixer(builder.note_builder, False)
-    result = fixer.try_change(note.info, transaction)
+def test_id_refuse_existing_conflict(fixture):
+    file = "/fix/missing-id-conflict.md"
+    fixer = IdFixer(fixture.index_builder.note_builder, False)
+    result = fixer.try_change(file, fixture.transact)
 
     assert result.is_failed
 
 
-def test_id_resolve_existing_conflict(transact_fixture):
-    provider, builder, master = transact_fixture
-    master.load_all()
-    transaction = master.create_empty_transaction()
+def test_id_resolve_existing_conflict(fixture):
+    file = "/fix/missing-id-conflict.md"
+    copy = deepcopy(fixture.master.get_note(file))
+    fixer = IdFixer(fixture.index_builder.note_builder, True)
+    result = fixer.try_change(file, fixture.transact)
+    fixture.transact.add_change(file, result.change)
+    fixture.master.apply_transaction(fixture.transact)
 
-    note = builder.note_builder.load_note("/fix/missing-id-conflict.md")
-    fixer = IdFixer(builder.note_builder, True)
-    result = fixer.try_change(note.info, transaction)
-    result.change.change_maker.apply_change(result.change)  # convoluted, but to ensure that it's stand-alone
-    note2 = builder.note_builder.load_note("/fix/missing-id-conflict.md")
+    note2 = fixture.index_builder.note_builder.load_note(file)
 
     assert result.is_ok
     assert note2.info.id == "20240102080136"
     assert note2.info.created == DateTime(2024, 1, 2, 8, 1, 36, tzinfo=local_tz)
-    assert note2.content == note.content
+    assert copy.content == note2.content
 
 
-def test_filename_check_true(check_fixture):
-    provider, builder = check_fixture
-    note = builder.note_builder.load_note("/alpha/note-00.md")
-    fixer = FilenameFixer(builder.note_builder, False)
+def test_filename_check_true(fixture):
+    note = fixture.master.get_note_info("/alpha/note-00.md")
+    fixer = FilenameFixer(fixture.index_builder.note_builder, False)
 
-    assert fixer.check(note.info)
-
-
-def test_filename_check_false(check_fixture):
-    provider, builder = check_fixture
-    note = builder.note_builder.load_note("/fix/19931114154205-risus-libero-id.md")
-    fixer = IdFixer(builder.note_builder, False)
-
-    assert not fixer.check(note.info)
+    assert fixer.check(note)
 
 
-def test_filename_nothing_to_do(transact_fixture):
-    provider, builder, master = transact_fixture
-    master.load_all()
-    transaction = master.create_empty_transaction()
+def test_filename_check_false(fixture):
+    note = fixture.master.get_note_info("/fix/19931114154205-risus-libero-id.md")
+    fixer = FilenameFixer(fixture.index_builder.note_builder, False)
 
-    note = builder.note_builder.load_note("/fix/19931114154205-risus-libero-id.md")
-    fixer = FilenameFixer(builder.note_builder, True)
-    result = fixer.try_change(note.info, transaction)
+    assert not fixer.check(note)
+
+
+def test_filename_nothing_to_do(fixture):
+    file = "/fix/19931114154205-risus-libero-id.md"
+    fixer = FilenameFixer(fixture.index_builder.note_builder, True)
+    result = fixer.try_change(file, fixture.transact)
 
     assert result.is_nothing
 
 
-def test_filename_simple_rename(transact_fixture):
-    provider, builder, master = transact_fixture
-    master.load_all()
-    transaction = master.create_empty_transaction()
+def test_filename_no_id(fixture):
+    file = "/fix/missing-id-only.md"
+    fixer = FilenameFixer(fixture.index_builder.note_builder, True)
+    result = fixer.try_change(file, fixture.transact)
 
-    note = builder.note_builder.load_note("/alpha/note-00.md")
-    fixer = FilenameFixer(builder.note_builder, False)
-    result = fixer.try_change(note.info, transaction)
-    result.change.change_maker.apply_change(result.change)  # convoluted, but to ensure that it's stand-alone
-    master.load_all()
-
-    assert note.info.file_path not in master.indices["alpha"].notes
-    note2 = builder.note_builder.load_note("/alpha/20240102080135-note-00.md")
-
-    assert result.is_ok
-    assert but_for(note2.info, "file_path") == but_for(note.info, "file_path")
-    assert note2.content == note.content
+    assert result.is_failed
 
 
-def test_filename_complete_rename(transact_fixture):
-    provider, builder, master = transact_fixture
-    master.load_all()
-    transaction = master.create_empty_transaction()
+def test_filename_empty_title(fixture):
+    file = "/fix/no-title-but-heading.md"
+    fixer = FilenameFixer(fixture.index_builder.note_builder, True)
+    result = fixer.try_change(file, fixture.transact)
 
-    note = builder.note_builder.load_note("/alpha/note-00.md")
-    fixer = FilenameFixer(builder.note_builder, True)
-    result = fixer.try_change(note.info, transaction)
-    result.change.change_maker.apply_change(result.change)  # convoluted, but to ensure that it's stand-alone
-    master.load_all()
+    assert result.is_failed
 
-    assert note.info.file_path not in master.indices["alpha"].notes
-    note2 = builder.note_builder.load_note("/alpha/20240102080135-auctor-neque-vitae.md")
+
+def test_filename_simple_rename(fixture):
+    file = "/alpha/note-00.md"
+    copy = deepcopy(fixture.master.get_note(file))
+    fixer = FilenameFixer(fixture.index_builder.note_builder, False)
+    result = fixer.try_change(file, fixture.transact)
+    fixture.transact.add_change(file, result.change)
+    fixture.master.apply_transaction(fixture.transact)
+
+    fixture.master.load_all()
+    assert file not in fixture.master.indices["alpha"].notes
+    note2 = fixture.index_builder.note_builder.load_note("/alpha/20240102080135-note-00.md")
 
     assert result.is_ok
-    assert but_for(note2.info, "file_path") == but_for(note.info, "file_path")
-    assert note2.content == note.content
+    assert but_for(note2.info, "file_path") == but_for(copy.info, "file_path")
+    assert note2.content == copy.content
 
 
-def test_author_check_true(check_fixture):
-    provider, builder = check_fixture
-    note = builder.note_builder.load_note("/fix/author.md")
-    fixer = AuthorFixer(builder.note_builder, "Irene Irenski")
+def test_filename_complete_rename(fixture):
+    file = "/alpha/note-00.md"
+    copy = deepcopy(fixture.master.get_note(file))
+    fixer = FilenameFixer(fixture.index_builder.note_builder, True)
+    result = fixer.try_change(file, fixture.transact)
+    fixture.transact.add_change(file, result.change)
+    fixture.master.apply_transaction(fixture.transact)
 
-    assert fixer.check(note.info)
+    fixture.master.load_all()
+    assert file not in fixture.master.indices["alpha"].notes
+    note2 = fixture.index_builder.note_builder.load_note("/alpha/20240102080135-auctor-neque-vitae.md")
+
+    assert result.is_ok
+    assert but_for(note2.info, "file_path") == but_for(copy.info, "file_path")
+    assert note2.content == copy.content
 
 
-def test_author_check_false(check_fixture):
-    provider, builder = check_fixture
-    note = builder.note_builder.load_note("/alpha/note-00.md")
-    fixer = AuthorFixer(builder.note_builder, "Irene Irenski")
+def test_author_check_true(fixture):
+    note = fixture.master.get_note_info("/fix/author.md")
+    fixer = AuthorFixer(fixture.index_builder.note_builder, "Irene Irenski")
 
-    assert not fixer.check(note.info)
+    assert fixer.check(note)
 
 
-def test_author_fix(check_fixture):
-    provider, builder = check_fixture
-    note = builder.note_builder.load_note("/fix/author.md")
-    fixer = AuthorFixer(builder.note_builder, "Irene Irenski")
-    result = fixer.try_change(note.info, ChangeTransaction())
+def test_author_check_false(fixture):
+    note = fixture.master.get_note_info("/alpha/note-00.md")
+    fixer = AuthorFixer(fixture.index_builder.note_builder, "Irene Irenski")
 
-    result.change.change_maker.apply_change(result.change)  # convoluted, but to ensure that it's stand-alone
-    note2 = builder.note_builder.load_note("/fix/author.md")
+    assert not fixer.check(note)
+
+
+def test_author_fix(fixture):
+    file = "/fix/author.md"
+    copy = deepcopy(fixture.master.get_note(file))
+    fixer = AuthorFixer(fixture.index_builder.note_builder, "Irene Irenski")
+    result = fixer.try_change(file, fixture.transact)
+    fixture.transact.add_change(file, result.change)
+    fixture.master.apply_transaction(fixture.transact)
+
+    note2 = fixture.index_builder.note_builder.load_note(file)
 
     assert result.is_ok
     assert note2.info.author == "Irene Irenski"
-    assert but_for(note.info, "author") == but_for(note2.info, "author")
-    assert note.content == note2.content
+    assert but_for(copy.info, "author") == but_for(note2.info, "author")
+    assert copy.content == note2.content
 
 
-def test_title_check_true(check_fixture):
-    provider, builder = check_fixture
-    note = builder.note_builder.load_note("/fix/no-title-but-heading.md")
-    fixer = TitleFixer(builder.note_builder)
+def test_title_check_true(fixture):
+    note = fixture.master.get_note_info("/fix/no-title-but-heading.md")
+    fixer = TitleFixer(fixture.index_builder.note_builder)
 
-    assert fixer.check(note.info)
-
-
-def test_title_check_false(check_fixture):
-    provider, builder = check_fixture
-    note = builder.note_builder.load_note("/alpha/note-00.md")
-    fixer = TitleFixer(builder.note_builder)
-
-    assert not fixer.check(note.info)
+    assert fixer.check(note)
 
 
-def test_title_fix_from_heading(check_fixture):
-    provider, builder = check_fixture
-    note = builder.note_builder.load_note("/fix/no-title-but-heading.md")
-    fixer = TitleFixer(builder.note_builder)
-    result = fixer.try_change(note.info, ChangeTransaction())
+def test_title_check_false(fixture):
+    note = fixture.master.get_note_info("/alpha/note-00.md")
+    fixer = TitleFixer(fixture.index_builder.note_builder)
 
-    result.change.change_maker.apply_change(result.change)  # convoluted, but to ensure that it's stand-alone
-    note2 = builder.note_builder.load_note("/fix/no-title-but-heading.md")
+    assert not fixer.check(note)
+
+
+def test_title_fix_from_heading(fixture):
+    file = "/fix/no-title-but-heading.md"
+    copy = deepcopy(fixture.master.get_note(file))
+    fixer = TitleFixer(fixture.index_builder.note_builder)
+    result = fixer.try_change(file, fixture.transact)
+    fixture.transact.add_change(file, result.change)
+    fixture.master.apply_transaction(fixture.transact)
+
+    note2 = fixture.index_builder.note_builder.load_note(file)
 
     assert result.is_ok
     assert note2.info.title == "At quis risus"
-    assert but_for(note.info, "title") == but_for(note2.info, "title")
-    assert note.content == note2.content
+    assert but_for(copy.info, "title") == but_for(note2.info, "title")
+    assert copy.content == note2.content
 
 
-def test_title_cant_fix_no_heading(check_fixture):
-    provider, builder = check_fixture
-    note = builder.note_builder.load_note("/fix/no-title-or-heading.md")
-    fixer = TitleFixer(builder.note_builder)
-    result = fixer.try_change(note.info, ChangeTransaction())
+def test_title_cant_fix_no_heading(fixture):
+    file = "/fix/no-title-or-heading.md"
+    fixer = TitleFixer(fixture.index_builder.note_builder)
+    result = fixer.try_change(file, fixture.transact)
 
     assert result.is_failed
