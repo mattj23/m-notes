223449abedf29670ba3b0119d880ec9bb557f3ff
Matt Jarvis <mattj23@gmail.com>
2021-02-22 03:39:10 -0500

    update version and documents

--- mnotes/cmd_backlink.py
+++ mnotes/cmd_backlink.py
@@ -18,7 +18,7 @@ def main(env: MnoteEnvironment, ctx: click.core.Context):
     start_time = time.time()
     env.global_index.load_all()
     end_time = time.time()
-    echo_line(style.success(f" * updated global index, took {end_time - start_time:0.2f} seconds"))
+    echo_line(style.success(f" * updated global directory, took {end_time - start_time:0.2f} seconds"))
 
 
 @main.command(name="gen")
